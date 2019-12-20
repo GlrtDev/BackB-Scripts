7b06cba32aeaea0e061d823c0980d4346a9894ea
GlrtDev <subin7777777@gmail.com>
2019-12-20 11:20:38 +0100

    Add LevelUI and LevelManagerUI

---
+++ Assets/Scripts/LevelManagerUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagerUI : MonoBehaviour
+{
+    public LevelManager levelManager;
+    public Transform content;
+    public LevelUI LevelUIPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (levelManager)
+            Display(levelManager);
+    }
+    public virtual void Display(LevelManager lm)
+    {
+        this.levelManager = lm;
+        Refresh();
+    }
+    public virtual void Refresh()
+    {
+        foreach (Transform t in content)
+        {
+            Destroy(t.gameObject);
+        }
+            foreach (Level lvl in levelManager.levels)
+        {
+            LevelUI ui = LevelUI.Instantiate(LevelUIPrefab, content);
+            ui.Display(lvl);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.Events;
+using TMPro;
+
+[System.Serializable]
+public class LevelUiEvent : UnityEvent<LevelUI> {}
+
+public class LevelUI : MonoBehaviour
+{
+    public Level level;
+    public int index;
+    public TMP_Text title;
+
+    public LevelUiEvent e;
+    void Start()
+    {
+        if (level)
+            Display(level);
+    }
+
+    public virtual void Display(Level lvl)
+    {
+        this.level = lvl;
+        this.index = lvl.index;
+        this.title.text = lvl.title;
+    }
+    public virtual void Click()
+    {
+
+    }
+}
