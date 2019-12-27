d2afbe7f7156376b3226c023a6f7cfce5b568521
GlrtDev <subin7777777@gmail.com>
2019-12-27 22:09:40 +0100

    Add IngameUI and MenuUI

---
+++ Assets/Scripts/GameLogic/IngameUI.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class IngameUI : MonoBehaviour
+{
+    public LevelGenerator levelGenerator;
+    public GameObject summaryUI;
+    public GameObject controlButtons;
+    public TMP_Text ballLeftUI;
+    public GameObject stars;
+    public Image starsFill;
+    //public static PlayerUIEvent playerUIEvent;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        UpdateText();
+        
+    }
+
+    public void ShowSummaryUI()
+    {
+        starsFill.fillAmount = 0.33f * LevelGenerator.StarsAcquired();
+        summaryUI.SetActive(true);
+    }
+
+    public void NextLevelClick()
+    {
+        HideSummaryUI();
+        levelGenerator.NextMap();
+    }
+
+    private void HideSummaryUI() //TO DO MAKE ANIMATIONS FOR UI
+    {
+        summaryUI.SetActive(false);
+    }
+
+    public void RestartClick()
+    {
+        HideSummaryUI();
+        levelGenerator.RestartLevel();
+    }
+
+    public void UpdateText()
+    {
+        ballLeftUI.text = "Balls Left : " + PlayerBehavoir.BallsLeft().ToString();
+       // Debug.Log("apdejt");
+    }
+
+    public void GoToMenu()
+    {
+        StartCoroutine(LoadMenuScene());
+    }
+
+    IEnumerator LoadMenuScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Menu Scene");
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/GameLogic/MenuUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BayatGames.SaveGameFree;
+
+public class MenuUI : MonoBehaviour
+{
+    public GameObject levelSelector;
+    public GameObject Menu;
+    public void GoToLevelSelection()
+    {
+        Menu.SetActive(false);
+        levelSelector.SetActive(true);
+    }
+
+    public void NewGame()
+    {
+        SaveData data = new SaveData();
+        SaveGame.Save<SaveData>("data", data);
+        PlayerData.starNumberPerLevel = data.starNumberPerLevel;
+    }
+}
