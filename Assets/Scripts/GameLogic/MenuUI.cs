e935aff4ce61c885ca7f5913547e4443070fd521
GlrtDev <subin7777777@gmail.com>
2020-01-19 23:46:46 +0100

    expand MenuUI

--- Assets/Scripts/GameLogic/MenuUI.cs
+++ Assets/Scripts/GameLogic/MenuUI.cs
@@ -6,13 +6,50 @@ using BayatGames.SaveGameFree;
 public class MenuUI : MonoBehaviour
 {
     public GameObject levelSelector;
-    public GameObject Menu;
+    public GameObject menu;
+    public GameObject playerSettings;
+    public GameObject settings;
+    public GameObject credits;
+    public GameObject menuButton;
+
     public void GoToLevelSelection()
     {
-        Menu.SetActive(false);
-        levelSelector.SetActive(true);
+        DisableAll();
+        levelSelector.SetActive(true); menuButton.SetActive(true);
     }
 
+    public void GoToPlayerSettings()
+    {
+        DisableAll();
+        playerSettings.SetActive(true); menuButton.SetActive(true);
+    }
+
+    public void GoToSettings()
+    {
+        DisableAll();
+        settings.SetActive(true); menuButton.SetActive(true);
+    }
+
+    public void GoToCredits()
+    {
+        DisableAll();
+        credits.SetActive(true); menuButton.SetActive(true);
+    }
+
+    public void GoToMenu()
+    {
+        DisableAll();
+        menu.SetActive(true); menuButton.SetActive(false); 
+    }
+
+    public void DisableAll()
+    {
+        settings.SetActive(false);
+        menu.SetActive(false);
+        credits.SetActive(false);
+        levelSelector.SetActive(false);
+        playerSettings.SetActive(false);
+    }
     public void NewGame()
     {
         SaveData data = new SaveData();
