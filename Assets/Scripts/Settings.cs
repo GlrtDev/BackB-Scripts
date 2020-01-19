c40af4b8eb7ac8a5a8ef096a3a6582a48e9f05ec
GlrtDev <subin7777777@gmail.com>
2020-01-20 00:49:53 +0100

    Add settings script

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject strikingGO;
+
+    public void SoundButtonClick()
+    {
+        if(AudioListener.volume == 1f)
+        {
+            strikingGO.SetActive(true); //striking visible
+            AudioListener.volume = 0f;
+        }
+        else
+        {
+            strikingGO.SetActive(false);
+            AudioListener.volume = 1f;
+        }
+
+    }
+    private void OnEnable()
+    {
+        if (AudioListener.volume == 0f)
+        {
+            strikingGO.SetActive(true);
+        }
+    }
+
+}
