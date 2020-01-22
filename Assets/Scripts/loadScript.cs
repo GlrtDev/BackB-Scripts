8ddac72960f69be015c2db9ccd95a3c8d95ecf71
GlrtDev <subin7777777@gmail.com>
2020-01-22 01:09:24 +0100

    Add load script

---
+++ Assets/Scripts/loadScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BayatGames.SaveGameFree;
+public class loadScript : MonoBehaviour
+{
+    void Awake()
+    {
+        SaveData data = SaveGame.Load<SaveData>("data", new SaveData());
+        PlayerData.starNumberPerLevel = data.starNumberPerLevel;
+        PlayerData.unlockedShapes = data.unlockedShapes;
+        PlayerData.unlockedTails = data.unlockedTails;
+    }
+    
+}
