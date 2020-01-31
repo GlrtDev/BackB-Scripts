3748ad04b229b18a8544e28aabd960c03599b89a
GlrtDev <subin7777777@gmail.com>
2020-01-31 20:28:14 +0100

    Add player equip script

---
+++ Assets/Scripts/GameLogic/PlayerSkin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkin : MonoBehaviour
+{
+    public TrailRenderer trailRenderer;
+    public MeshFilter meshFilter;
+    private void OnEnable()
+    {
+        if (PlayerData.currentTail != null)
+            trailRenderer.material = PlayerData.currentTail;
+        else
+            Debug.Log("no renderer");
+
+        if (PlayerData.currentShape != null)
+            meshFilter.mesh = PlayerData.currentShape;
+        else
+            Debug.Log("no renderer");
+    }
+}
