d7e1a69d498f71b5d0b676440f9a205d75ef364f
GlrtDev <subin7777777@gmail.com>
2020-02-12 21:20:06 +0100

    change TailManagerUi to ItemManagerUI

---
+++ Assets/Scripts/UI/ItemManagerUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManagerUI : MonoBehaviour
+{
+    public ItemManager itemManager;
+    public Transform content;
+    void Start()
+    {
+        if (itemManager)
+            Display(itemManager);
+    }
+    public virtual void Display(ItemManager tm)
+    {
+        this.itemManager = tm;
+        Refresh();
+    }
+
+    public virtual void Refresh()
+    {
+        foreach (Transform t in content)
+        {
+            Destroy(t.gameObject);
+        }
+
+        foreach(GameObject itemPrefab in itemManager.itemPrefabs)
+        {
+            GameObject Item = Instantiate(itemPrefab, content);
+            Item.GetComponent<Tail>().ItemEquipedEvent += Refresh;
+        }
+    }
+}
