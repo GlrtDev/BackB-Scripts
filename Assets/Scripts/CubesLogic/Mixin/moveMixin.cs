e50ec8cb5169560b64b1e4257f95f44234b521d6
GlrtDev <subin7777777@gmail.com>
2020-03-01 20:54:36 +0100

    Add delay to moveMixin

--- Assets/Scripts/CubesLogic/Mixin/moveMixin.cs
+++ Assets/Scripts/CubesLogic/Mixin/moveMixin.cs
@@ -4,12 +4,13 @@ public class moveMixin : CubeMixin
 {
     public Vector3 moveVector = Vector3.zero;
     public float speed = 20.0f;
+    public float delay = 0.05f;
     public override void LoadMixin()
     {
         iTween.MoveBy(cube.gameObject, iTween.Hash(
             "amount", moveVector,
             "speed", speed,
-            "delay", 0.05f,
+            "delay", delay,
             "easetype", iTween.EaseType.easeInOutExpo,
             "looptype", iTween.LoopType.pingPong
             ));
