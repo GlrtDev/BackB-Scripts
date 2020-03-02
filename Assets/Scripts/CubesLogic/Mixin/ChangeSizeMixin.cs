3b599e22a9061a9810d057a5cefcce398087be7a
GlrtDev <subin7777777@gmail.com>
2020-03-02 23:29:46 +0100

    Add changeSize cubeMixin

---
+++ Assets/Scripts/CubesLogic/Mixin/ChangeSizeMixin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSizeMixin : CubeMixin
+{
+    public Vector3 scaleVector = Vector3.zero;
+    public float speed = 20.0f;
+    public float delay = 1.5f;
+    public bool CheckForUpdate = false;
+    public override void LoadMixin()
+    {
+        iTween.ScaleTo(cube.gameObject, iTween.Hash(
+            "scale", scaleVector,
+            "speed", speed,
+            "delay", delay,
+            "easetype", iTween.EaseType.easeInOutExpo,
+            "looptype", iTween.LoopType.pingPong
+            ));
+    }
+    //TO DO , THIS IS TOO SLOW 
+    public void Update()
+    {
+        if (CheckForUpdate)
+        {
+            if (cube.gameObject.transform.localScale.x < 0.1f)
+                cube.gameObject.GetComponent<BoxCollider>().enabled = false;
+            else
+                cube.gameObject.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}
